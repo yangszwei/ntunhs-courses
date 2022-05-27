14ace20c52403440757d647e0e9c91a0d73612aa
楊斯惟 <yangszwei@outlook.com>
2022-05-27 13:22:09 +0800

    Set up SvelteKit project

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $components: 'src/components',
+        }
+    },
+};
+
+export default config;
