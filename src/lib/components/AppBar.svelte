034c9a29c8588010d463755518bac56a77ac2de4
楊斯惟 <yangszwei@outlook.com>
2022-06-09 19:44:13 +0800

    Update AppBar component

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -3,11 +3,12 @@
 	export let title = '';
 </script>
 
-<nav class="fixed top-0 left-0 right-0 bg-gray-100">
+<header class="fixed top-0 left-0 right-0">
 	<div class="flex h-14 items-center">
-		<h1 class="ml-4 text-xl font-medium">{title}</h1>
+		<h1 class="mx-2 grow pl-2 text-xl font-medium">{title}</h1>
+		<div class="mx-2 shrink-0"><slot /></div>
 	</div>
-</nav>
+</header>
 
 <style>
 	:global(body) {
